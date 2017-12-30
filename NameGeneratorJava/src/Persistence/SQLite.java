cd2c64a74082abfc277e3d62caab628969398516
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2017-12-30 12:12:56 +0000

    Insert names
    Implemented the way we can insert names into our database.

---
+++ NameGeneratorJava/src/Persistence/SQLite.java
@@ -0,0 +1,72 @@
+package Persistence;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class SQLite {
+    public Connection connect(){
+        String url = "jdbc:sqlite:../names.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            System.out.println("Can't connect to the db.");
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public ArrayList<String> getRandomFullName(Connection connection, Statement statement) throws SQLException {
+        if(connection == null)return new ArrayList<>();
+        ArrayList result= new ArrayList<String>();
+        String nameQuery = "SELECT name FROM names ORDER BY RANDOM() LIMIT 1";
+        String lastnameQuery = "SELECT lastname FROM lastnames ORDER BY RANDOM() LIMIT 1";
+        ResultSet resultSet =  statement.executeQuery(nameQuery);
+        result.add(resultSet.getString("name"));
+        resultSet = statement.executeQuery(lastnameQuery);
+        result.add(resultSet.getString("lastname"));
+
+        resultSet.close();
+
+        return result;
+    }
+
+    public boolean insertData(Connection connection, String name, String lastname) {
+        if(connection == null)return false;
+        Boolean result = true;
+        if(!insert(connection, "names(name)", name))result = false;
+        if(!insert(connection, "lastnames(lastname)", lastname))result = false;
+        if(!result){
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                connection.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    private boolean insert(Connection connection, String table, String data){
+        if(!data.equals("")){
+            String query = "INSERT INTO " + table + " VALUES(?)";
+            System.out.println(query);
+            PreparedStatement preparedStatement;
+            try{
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, data);
+                preparedStatement.executeUpdate();
+            }catch (SQLException e){
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- NameGeneratorJava/src/main.java
+++ NameGeneratorJava/src/main.java
@@ -1,23 +1,26 @@
 import Controller.FullNameController;
 import Model.FullName;
+import Persistence.SQLite;
 import View.FullNameView;
 import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class main {
+    static SQLite sqLite = new SQLite();
 
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Name generator");
-        mainFrame.setLayout(new GridLayout(2,1));
+        mainFrame.setLayout(new BorderLayout());
 
         FullName model = new FullName();
         FullNameView view = new FullNameView();
         FullNameController controller = new FullNameController(view, model);
 
-        mainFrame.add(view);
+        mainFrame.add(view, BorderLayout.NORTH);
 
         JButton generateNameButton = new JButton("Generate!");
         generateNameButton.addActionListener(new ActionListener() {
@@ -28,8 +31,27 @@ public class main {
                 controller.updateView();
             }
         });
-
-        mainFrame.add(generateNameButton);
+        JButton addToDatabaseButton = new JButton("Insert new names / lastnames");
+        addToDatabaseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTextField firstName = new JTextField();
+                JTextField lastName = new JTextField();
+                final JComponent[] inputs = new JComponent[] {
+                        new JLabel("Name"),
+                        firstName,
+                        new JLabel("Lastname"),
+                        lastName,
+                };
+                int result = JOptionPane
+                        .showConfirmDialog(null, inputs, "Insert to database", JOptionPane.PLAIN_MESSAGE);
+                if (result == JOptionPane.OK_OPTION) {
+                    insertData(firstName.getText(), lastName.getText());
+                }
+            }
+        });
+        mainFrame.add(generateNameButton, BorderLayout.CENTER);
+        mainFrame.add(addToDatabaseButton, BorderLayout.SOUTH);
         mainFrame.pack();
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setVisible(true);
@@ -37,28 +59,30 @@ public class main {
 
     private static FullName retrieveNewFullName() {
         FullName fullName = new FullName();
-        Connection connection;
         Statement statement;
         try{
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:../names.db");
-            connection.setAutoCommit(false);
-
+            Connection connection = sqLite.connect();
             statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("SELECT name FROM names ORDER BY RANDOM() LIMIT 1");
-            fullName.setName(resultSet.getString("name"));
-            resultSet = statement.executeQuery("SELECT lastname FROM lastnames ORDER BY RANDOM() LIMIT 1");
-            fullName.setLastName(resultSet.getString("lastname"));
+            List<String> randomFullName = sqLite.getRandomFullName(connection, statement);
+            if(!randomFullName.isEmpty()){
+                fullName.setName(randomFullName.get(0));
+                fullName.setLastName(randomFullName.get(1));
+            }
 
-            resultSet.close();
             statement.close();
             connection.close();
 
             System.out.println(fullName);
         }catch (Exception e){
             System.out.println("Something went wrong");
+            System.out.println(e.getMessage());
         }
         return fullName;
+    }
+
+    private static void insertData(String name, String lastname){
+        Connection connection = sqLite.connect();
+        if(sqLite.insertData(connection, name, lastname)) JOptionPane.showMessageDialog(null, "Done!");
     }
 }
